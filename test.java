e97708367635c5fd875c7ec532f286b68318db3d
Esra'a Alsaedi <124468353+EsraaAlsaedi@users.noreply.github.com>
2023-06-06 02:08:20 +0300

    Add files via upload
    Added body to the doctor class

--- test.java
+++ test.java
@@ -16,18 +16,77 @@ class Hospital {
 	
 }
 interface Person{
-	 String name = null ;
+	 
+     static final String name = null;
+     static final int id = 0;
+	 static final String email = null;
+
+	 abstract String getName();
+	 abstract void setName(String name);
+	 abstract int getID();
+	 abstract void setID(int id);
+	 abstract String getEmail();
+	 abstract void setEmail(String email);
 }
 abstract class Staff implements Person{
+    private String staffName;
+    private int staffId;
+    private String staffEmail;
+    private String userName;
+    protected String password;
+
+    public Staff() {
+      
+    }
+
+    public Staff(String username, String password) {
+        this.userName=username;
+        this.password=password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String name) {
+        this.userName = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return staffName;
+    }
+
+    public void setName(String name) {
+         staffName = name;
+    }
+    public int getID(){
+        return staffId;
+    }
+    public void setID(int id){
+        staffId = id;
+    }
+    public String getEmail(){
+        return staffEmail;
+    }
+    public void setEmail(String email){
+        staffEmail = email;
+    }
 	  
-	  
-	}
+}
 	 
 class Patient extends Appointment implements Person{
 	    private String patientName;
 	    private int age;
-	    private int id;
-	    private String email;
+	    private int patientId;
+	    private String patientEmail;
 	    private double weight;
 	    private double height;
 	    private String bloodType;
@@ -42,8 +101,8 @@ class Patient extends Appointment implements Person{
 		public Patient(String name,int age,int id,String email,double weight,double height,String bloodType) {
 			this.patientName = name;
 			this.age = age;
-			this.id=id;
-			this.email=email;
+			this.patientId=id;
+			this.patientEmail=email;
 			this.weight=weight;
 			this.height=height;
 			this.bloodType=bloodType;
@@ -59,19 +118,19 @@ class Patient extends Appointment implements Person{
 		}
 		
 		public int getID() {
-			return this.id;
+			return this.patientId;
 		}
 		
 		public void setID(int id) {
-			this.id=id;
+			this.patientId=id;
 		}
 		
 		public String getEmail() {
-			return email;
+			return patientEmail;
 		}
 		
 		public void setEmail(String email) {
-			this.email=email;
+			this.patientEmail=email;
 		}
 		
 		public int getAge() {
@@ -124,21 +183,21 @@ class Patient extends Appointment implements Person{
 		
 		@Override
 		public String toString() {
-			return "The patient name: "+ patientName + ", age: "+ age +", id: "+id
-					+", email: "+email+", bloodType: "+ bloodType+", weight: "+weight+", and height: "+height;
+			return "The patient name: "+ patientName + ", age: "+ age +", id: "+ patientId
+					+", email: "+patientEmail+", bloodType: "+ bloodType+", weight: "+weight+", and height: "+height+".";
 		}
 		public void calculateTotalBMI(double weight,double height) {
 			double BMI = weight/(height*height);
 			
 			if(BMI<18.5) {
-				System.out.println("Underweight");
+				System.out.println("Underweight.");
 			}
 			    
 			else if (BMI>25 | BMI==25) {
-				System.out.println("Overweight");
+				System.out.println("Overweight.");
 			}
 			else
-				System.out.println("Healthy");
+				System.out.println("Healthy.");
 			
 			}
 		
@@ -146,23 +205,86 @@ class Patient extends Appointment implements Person{
 			return numberOfPatientObjects;
 		}
 	}
+
 class Doctor extends Staff{ 
 	  
 	   private ArrayList<Patient> patients; //aggregation relationship
+	   private String doctorName; 
+	   private int doctorId;
+	   private String specialty;
+	   private int numberOfDoctorObjects;
 	   
-	   private static int doctorObj = 0;
+	   public Doctor(){
+	       numberOfDoctorObjects++;
+	    }
 	   
-	   public Doctor() {
-		   doctorObj++;
-	   }
-	   
-	   public String prescribe(Patient patient,Doctor doctor) {
-		   return null;
-	   }
-	   
-	   public static int getNumberofObjects() {
-		   return doctorObj;
-	   }
+	    public Doctor(String name,int id,String specialty){
+	    	setDoctorName(name);
+	    	setDoctorId(id);
+	        this.specialty=specialty;
+	        numberOfDoctorObjects++;
+	    }
+	    
+	    public String getDoctorSpecialty(){
+	        return this.specialty;
+	        
+	    }
+	    
+	    public void setDoctorSpecialty(String specialty){
+	        this.specialty=specialty;
+	    }
+	    
+	    public ArrayList<Patient> getPatients(){
+			return this.patients;
+		}
+		
+		public void setPatients(ArrayList<Patient> patients){
+			 this.patients=patients;
+		}
+		
+		public void addPatient(String patient) {
+			ArrayList<String> ptn = new ArrayList();
+			if(ptn.contains(patient)) {
+				System.out.println("This patient is already registered.");
+			}
+			else
+			ptn.add(patient);
+		}
+		
+		public void deletePatient(String patient) {
+			ArrayList<String> ptn = new ArrayList();
+			if(!(ptn.isEmpty())) {
+			ptn.remove(patient);
+			}
+			else
+				System.out.println("There is no patient registered is this name.");
+		}
+		
+		public String getDoctorName() {
+			return doctorName;
+		}
+
+		public void setDoctorName(String name) {
+			this.doctorName = name;
+		}
+		
+	    public int getDoctorId() {
+			return doctorId;
+		}
+
+		public void setDoctorId(int id) {
+			this.doctorId = id;
+		}
+
+		public int getNumberOfDoctorObjects(){
+	        return numberOfDoctorObjects;
+	    }
+		
+		@Override
+	    public String toString(){
+	        return "The doctor name: "+ doctorName +", id: "+ doctorId +" and specialty: "+ specialty+".";
+	        
+	    }
 	}
 	
 class Nurse extends Staff{
@@ -211,7 +333,7 @@ class Appointment extends Hospital{
 	public void addAppintment(String day) {
 		ArrayList<String> appnts = new ArrayList();
 		if(appnts.contains(day)) {
-			System.out.println("There is already an appointment in this day");
+			System.out.println("There is already an appointment in this day.");
 		}
 		else
 		appnts.add(day);
@@ -223,10 +345,10 @@ class Appointment extends Hospital{
 		appnts.remove(appnts);
 		}
 		else
-			System.out.println("There is no appointments in this day, try again.");
+			System.out.println("There is no appointments in this day.");
 	}
 	public String toString() {
-		return "The appointment number is: "+appointmentNumber;
+		return "The appointment number is: "+appointmentNumber+".";
 	  }
 	public static int getNumberOfAppointmentObjects() {
 		return numberOfAppointmentObjects;
